bb8ce1d5481106eee7d17c5cba5cea53d8c37839
Sonia <105906605+SoniaSatam@users.noreply.github.com>
2024-02-13 23:30:04 +0530

    Add files via upload

---
+++ palindromeString.py
@@ -0,0 +1,14 @@
+class Solution:
+    def firstPalindrome(self, words: list[str]) -> str:
+        for word in words:
+            if word == word[::-1]:
+                return word
+        return ""
+    
+
+words = ["abc", "car", "ada", "racecar", "cool"]
+
+solution = Solution()
+
+
+print(solution.firstPalindrome(words))
